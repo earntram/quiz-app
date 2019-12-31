bd9d9745c262489ae17765c0ea7ccdd53ac312bc
earntram <53387455+earntram@users.noreply.github.com>
2019-12-31 14:24:14 -0700

    Create store.js

---
+++ store.js
@@ -0,0 +1,61 @@
+const STORE = {
+  questions: [//1
+    {
+      question: "The ratio of the model car’s dimensions to the actual car’s dimensions are 1/56.  The model is 3 inches long.  What is the length of the actual car?",
+      options: [
+        "a) 5 ft", 
+        "b) 14 ft", 
+        "c) 100 ft", 
+        "d) 125 ft"
+      ],
+      answer: "b) 14 ft"
+    },
+    //2
+    {
+      question: "What is the area of a circle with the radius of 5 inches?",
+      options: [
+        "a) 58.3 in",
+        "b) 65.4 in", 
+        "c) 78.5 in",
+        "d) 100 in"
+      ],
+      answer: "c) 78.5 in"
+    },
+    //3
+    {
+      question: "What is the area of a square with the side length of 10 ft?",
+      options: [
+        "a) 5 ft", 
+        "b) 14 ft", 
+        "c) 100 ft", 
+        "d) 125 ft"
+      ],
+      answer: "c) 100 ft"
+    },
+    //4
+    {
+      question: "What is the volume of a cube with the side length of 5 ft?", 
+      options: [
+        "a) 5 ft", 
+        "b) 14 ft", 
+        "c) 100 ft", 
+        "d) 125 ft"
+        ],
+      answer: "d) 125 ft"
+    },
+    //5
+    {
+      question: "What is the volume of a square pyramid with the side length of 5 in and height of 7 in?",
+      options: [
+        "a) 58.3 in", 
+        "b) 65.4 in", 
+        "c) 78.5 in", 
+        "d) 100 in"
+      ],
+      answer: "a) 58.3 in"
+    },
+
+  ],
+  currentQuestion: 0,
+  score: 0
+};
